97793f2cfc94e4cca86e5b803a6aef15e50d711c
Ganhart <ganhart2@gmail.com>
2021-05-02 16:42:57 +0300

    additional check in healthchecker

--- health_checker.sh
+++ health_checker.sh
@@ -4,6 +4,11 @@ set -e
 
 DOMAIN=$DOMAIN_SUFFIX
 
+if [ -z "$DOMAIN" ]; then
+	echo "Missing domain, please pass it via env"
+	exit 1
+fi
+
 LOG_RESULT=$(cat /var/log/letsencrypt/letsencrypt.log | grep -ci "Nginx reload failed") || [ $? -eq 1 ]
 
 if [ $LOG_RESULT != 0 ]; then
